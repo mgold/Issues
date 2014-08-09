378d95c1b7831fcf632df655cd5245243e2f00ab
Max Goldstein <maxgoldstein1@gmail.com>
2014-08-09 00:04:02 -0400

    Bootstrap?

--- server.rb
+++ server.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'json'
 
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+disable :static
+
+get "/public/:asset" do |asset|
+  send_file File.join(settings.public_folder, asset)
+end
+
 get '/:user/:repo' do |user, repo|
   json = File.read("data/srv/#{user}_#{repo}.json") rescue (return 404)
   data = JSON.parse(json) rescue (return 500)
